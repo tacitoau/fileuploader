e77de5675e8b17377de6c51ab99b9c9da492225e
t9fiction <sohail.sohailishaq@gmail.com>
2023-03-16 19:36:59 +0500

    file upload working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { create } from "ipfs-http-client";
+import "./App.css";
+import { useState } from "react";
+
+const { REACT_APP_projectId, REACT_APP_projectSecret } = process.env;
+
+// const client = ipfsHttpClient("https://ipfs.infura.io:5001/ap/v0");
+
+// authentication
+const auth =
+  "Basic " +
+  Buffer.from(REACT_APP_projectId + ":" + REACT_APP_projectSecret).toString(
+    "base64"
+  );
+
+const client = create({
+  host: "ipfs.infura.io",
+  port: 5001,
+  protocol: "https",
+  apiPath: "/api/v0",
+  headers: {
+    authorization: auth,
+  },
+});
 
 function App() {
+  const [file, setfile] = useState();
+
+  const handleClick = async () => {
+    console.log(file);
+    const add = await client.add(file);
+    const url = `https://ipfs.io/ipfs/${add.path}`;
+    console.log(url);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>File Uploader</h1>
+      <br />
+      <input
+        placeholder="Select the file"
+        type="file"
+        onChange={(e) => setfile(e.target.files[0])}
+      />
+      <br />
+      <button type="button" onClick={handleClick}>
+        Upload
+      </button>
     </div>
   );
 }
