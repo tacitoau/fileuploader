755d6df2ee3cf6661ec1006110913698b75ffa10
Tácito Augusto Farias Júnior <81604617+tacitoau@users.noreply.github.com>
2023-03-30 14:56:02 -0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,6 @@ import { ConnectWallet } from "@thirdweb-dev/react";
 export default function Navbar() {
   return (
     <div className="Nav">
-        <h2>Logo</h2>
       <ConnectWallet accentColor="#d3d3d3" colorMode="dark" />
     </div>
   );
